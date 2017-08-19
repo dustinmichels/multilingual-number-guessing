88bc0b7b56a0b3a9b5b4535987a328ee382060b4
Dustin Michels <dustin7538@gmail.com>
2017-08-19 01:21:16 -0600

    Create README.md

---
+++ python/README.md
@@ -0,0 +1,18 @@
+![Image of Python Logo](https://www.python.org/static/community_logos/python-logo-master-v3-TM.png)
+
+[Python](https://www.python.org/) is a free, open source programming language, popular for uses ranging from
+from simple scripting, to building web applications, to doing data science and machine learning.
+
+Created by Guido van Rossum and first released in 1991, Python is:
+- High level & general purpose
+- Dynamically typed
+- Interpreted (rather than compiled)
+- Supportive of multiple programming paradigms, including object-oriented,
+imperative, functional, and procedural
+
+## Running the program
+
+After [downloading Python 3.6+](https://www.python.org/downloads/release/python),
+you can run this file from the command line with:
+
+`python3 guess.py`
--- python/guess.py
+++ python/guess.py
@@ -1,6 +1,6 @@
 """
-Simple number guessing game for python 3.6+
-Run from terminal with 'python3 guess.py'
+Generates a (pseudo)random number between 1 and 100, then prompts
+the user for guesses until the correct number is found.
 
 Dustin Michels
 August 2017
@@ -10,34 +10,25 @@ August 2017
 import random
 
 
-def main():
-    """Generates a (pseudo)random number between 1 and 100, then prompts
-    the user for guesses until the correct number is found."""
+my_num = random.randrange(1, 100)
+num_guesses = 0
 
-    secret_num = random.randrange(1, 100)
-    num_guesses = 0
+print('\nHello! I\'m thinking of a number between 1 and 100. Can you guess it?')
 
-    print('\nHello! I\'m thinking of a number between 1 and 100. Can you guess it?')
+while True:
+    guess = input('Your guess: ')
 
-    while True:
-        guess = input('Your guess: ')
+    try:
+        guess = int(guess)
+    except ValueError:
+        print('Oops! Your guess must be an integer between 1 and 100!')
 
-        try:
-            guess = int(guess)
-        except ValueError:
-            print('Oops! Your guess must be an integer between 1 and 100!')
-
-        if guess < secret_num:
-            print("Higher...")
-            num_guesses += 1
-        elif guess > secret_num:
-            print("Lower...")
-            num_guesses += 1
-        else:
-            print(f'Yeah!! It was {secret_num}! Nice job!')
-            print(f'You got the right answer in {num_guesses} guesses.\n')
-            return
-
-
-if __name__ == "__main__":
-    main()
+    if guess < my_num:
+        print("Higher...")
+        num_guesses += 1
+    elif guess > my_num:
+        print("Lower...")
+        num_guesses += 1
+    else:
+        print(f'Yeah!! It was {secret_num}! Nice job!')
+        print(f'You got the right answer in {num_guesses} guesses.\n')
