793c5257c5570626e9d9c45d9be5b75ccb4f97dd
Dustin Michels <dustin7538@gmail.com>
2017-09-02 18:28:54 -0600

    Var name fix

--- Python/guess.py
+++ Python/guess.py
@@ -15,9 +15,9 @@ import random
 
 
 my_num = random.randrange(1, 100)
-num_guesses = 0
+guess_count = 0
 
-print('\nHello! I\'m thinking of a number between 1 and 100. Can you guess it?')
+print("\nHello! I'm thinking of a number between 1 and 100. Can you guess it?")
 
 while True:
     guess = input('Your guess: ')
@@ -28,11 +28,11 @@ while True:
         print('Oops! Your guess must be an integer between 1 and 100!')
 
     if guess < my_num:
-        print("Higher...")
-        num_guesses += 1
+        print('Higher...')
+        guess_count += 1
     elif guess > my_num:
-        print("Lower...")
-        num_guesses += 1
+        print('Lower...')
+        guess_count += 1
     else:
-        print(f'Yeah!! It was {secret_num}! Nice job!')
-        print(f'You got the right answer in {num_guesses} guesses.\n')
+        print(f'Yeah!! It was {my_num}! Nice job!')
+        print(f'You got the right answer in {guess_count} guesses.\n')
