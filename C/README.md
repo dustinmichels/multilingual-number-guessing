d6b15f6bcae335c6313acf4fb07ab34584dea1e0
Dustin Michels <dustin7538@gmail.com>
2017-10-25 04:44:04 -0500

    C (#2)
    * progress
    
    * update guess.c
    
    * update

---
+++ C/README.md
@@ -0,0 +1,10 @@
+![C logo](https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/The_C_Programming_Language_logo.svg/800px-The_C_Programming_Language_logo.svg.png?1508924421415)
+
+C is a low-level language, originally developed by Dennis Ritchie between 1969 and 1973 at Bell Labs.
+
+It is:
+
+* general-purpose
+* imperative
+* compiled
+* weakly typed
---
+++ C/guess.c
@@ -0,0 +1,62 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+# include <ctype.h>
+#include <errno.h>
+
+# define BUFFER 100
+
+int readGuess();
+void checkNumber();
+
+int main() {
+	int num, guess, max_size;
+	char input[BUFFER];
+	char *end;;
+
+	// Set max size of random number
+	max_size = 101;
+
+	// Generate random num (not the optimal way...)
+	srand(time(NULL));
+	num = rand() % max_size;
+
+	// Print welcome message
+	printf("Hi! I'm thinking of a number between 0 and 100. ");
+	printf("Can you guess it??\n");
+
+	// Temp
+	printf("...................The random number is: %d\n", num);
+
+	do {
+		printf("\nPlease enter your guess: ");
+
+		if (fgets(input, sizeof(input), stdin) != NULL) {
+
+			// reset errno
+			errno = 0;
+
+			// convert string to int
+			// If successful, check number.
+			guess = strtol(input, NULL, 10);
+			if (errno == EINVAL) {
+				printf("Oops! That doesn't look valid!\n");
+			} else {
+				checkNumber(guess, num);
+			}
+		}
+	} while (guess != num);
+
+	return 0;
+}
+
+void checkNumber(int guess, int num) {
+
+	if (guess < num) {
+		printf("Higher than %d...\n", guess);
+	} else if (guess > num) {
+		printf("Lower than %d...\n", guess);
+	} else if (guess == num) {
+		printf("Yeah! It was %d!\n\n", num);
+	}
+}
