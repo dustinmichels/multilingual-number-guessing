74599c3e12c99b0c3062caefdef7535dc18ac340
Dustin Michels <dustin7538@gmail.com>
2018-04-16 15:55:15 -0500

    improvements to guess.ex

--- Elixir/guess.ex
+++ Elixir/guess.ex
@@ -4,45 +4,36 @@ defmodule Guess do
   """
 
   @doc """
-  Prompts user for a guess until an integer is given.
-  Returns guess.
-  Q: is it tail call optimized? Should use try/rescue instead?
+  Chooses secret number at random, then calls guess_and_check.
+  Prints welcome/closing messages.
   """
-  def get_guess do
-    response = IO.gets "Guess a number between 1 and 100: "
-    case Integer.parse(response) do
-      {guess, _} -> guess
-      :error ->
-        IO.puts "  Oops. That doesn't look like an integer..."
+  def play do
+    secret_number = Enum.random(0..100)
+    IO.puts "Welcome to a number guessing game in... elixir!"
+    guess_count = guess_and_check(secret_number, 1)
+    IO.puts "Yeah!! The secret number was #{secret_number}."
+    IO.puts "You got it in #{guess_count} tries."
+  end
+
+  defp guess_and_check(secret_number, guess_count) do
+    guess = get_guess()
+    case guess do
+      ^secret_number -> guess_count
+      _ ->
+        msg = if guess < secret_number, do: "higher!", else: "lower!"
+        IO.puts "  You guessed #{guess}... Go #{msg}"
+        guess_and_check(secret_number, guess_count+1)
+    end
+  end
+
+  defp get_guess do
+    guess = Integer.parse(IO.gets "Guess a number between 1 and 100: ")
+    case guess do
+      {n, _} when n in 1..100 -> n
+      _ ->
+        IO.puts "  Oops. That doesn't look like a valid guess..."
         get_guess()
     end
   end
 
-
-  @doc """
-  Solicits user guess, then checks it against secret number.
-  Returns the number of guesses made
-  """
-  def guess_and_check(secret_number, guess_count) do
-    guess = get_guess()
-    if guess == secret_number do
-      guess_count
-    else
-      msg = if guess < secret_number, do: "higher!", else: "lower!"
-      IO.puts "  You guessed #{guess}... Go #{msg}"
-      guess_and_check(secret_number, guess_count + 1)
-    end
-  end
-
-  @doc """
-  Chooses secret number at random, sets off calls to `guess_and_check`.
-  Prints welcome/closing messages.
-  """
-  def start do
-    secret_number = Enum.random(0..100)
-    IO.puts "Welcome to a number guessing game in... elixir!"
-    guess_count = guess_and_check(secret_number, 0)
-    IO.puts "Yeah!! The secret number was #{secret_number}."
-    IO.puts "You got it in #{guess_count} tries."
-  end
 end
